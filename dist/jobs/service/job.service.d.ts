520104514ed5481f4c8b94b610c65137d8877b33
HardikKapil1 <kapilhardik11@gmail.com>
2026-06-28 16:40:31 +0530

    Refactor code structure and remove redundant sections for improved readability and maintainability

--- dist/jobs/service/job.service.d.ts
+++
@@ -1,7 +0,0 @@
-import { Job } from '../entities/job.entity';
-import { Repository } from 'typeorm';
-export declare class JobService {
-    private jobRepository;
-    constructor(jobRepository: Repository<Job>);
-    createJob(jobName: string, payload: Record<string, any>): Promise<Job>;
-}
